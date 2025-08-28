40fbd5f5a38ede47a699efc0e5aba3a600f6d518
Gautam kumar <gautamsiddharth2013@gmail.com>
2025-08-28 19:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-}
+	let freq = {};
+	for(i=0; i < str.length; i++){
+		let char  = str[i];
+		if(freq[char]){
+			freq[char] += 1
+		} else {
+			freq[char] = 1;
+		}
+	}
+	for(i=0; i < srt.lenth; i++){{
+		let char = str[i];
+		if(freq[char] === 1){
+			return char;
+		} else [
+			return null;
+		]
+	}
+} console.log(firstNonRepeatedChar());
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+	
